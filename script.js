382d76bfef1dd7ec3cddb78ba1040cae2993f8fb
Divyatmaj <ddivyatmaj@gmail.com>
2026-04-06 22:23:31 +0530

    added the next and prev pages functionality

--- script.js
+++ script.js
@@ -4,23 +4,48 @@ const root = document.getElementById("root");
 let allMovies = [];
 // 🔍 SEARCH FEATURE END
 
-async function fetchMovies() {
+// 📄 PAGINATION FEATURE START
+let currentPage = 1;
+// 📄 PAGINATION FEATURE END
+
+async function fetchMovies(page = 1) {
   try {
     //this is just so that the webste does not seem blank when the API is loading
     root.innerHTML = "<h2>Loading...</h2>";
     //other api with more queries for more accurate results
     // https://api.jikan.moe/v4/top/anime?limit=12&page=4
     
-    const response = await fetch("https://api.jikan.moe/v4/anime");
+    // 📄 PAGINATION FEATURE START
+    const response = await fetch(`https://api.jikan.moe/v4/anime?page=${page}`);
+    // 📄 PAGINATION FEATURE END
+
     const data = await response.json();
 
     root.innerHTML = "";
 
-    //  SEARCH FEATURE START
+    // 🔍 SEARCH FEATURE START
     allMovies = data.data;
     displayMovies(allMovies);
+    // 🔍 SEARCH FEATURE END
 
+    /*
+    data.data.forEach(movie => {
+      const card = document.createElement("div");
+      card.className = "card";
 
+      const img = document.createElement("img");
+      //three sizes available,I chose the larger one because it was the clearest one and the other two did not have great resolution
+      img.src = movie.images.jpg.large_image_url;
+
+      const title = document.createElement("h1");
+      title.textContent = movie.title;
+
+      card.appendChild(img);
+      card.appendChild(title);
+
+      root.appendChild(card);
+    });
+    */
 
   } catch (error) {
     root.innerHTML = "<h2>Failed to load data</h2>";
@@ -62,4 +87,23 @@ document.getElementById("searchInput").addEventListener("input", (e) => {
 });
 // 🔍 SEARCH FEATURE END
 
-fetchMovies();
+
+// 📄 PAGINATION FEATURE START
+
+// NEXT BUTTON
+document.getElementById("nextBtn").addEventListener("click", () => {
+  currentPage++;
+  fetchMovies(currentPage);
+});
+
+// PREVIOUS BUTTON
+document.getElementById("prevBtn").addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    fetchMovies(currentPage);
+  }
+});
+
+// 📄 PAGINATION FEATURE END
+
+fetchMovies(currentPage);
